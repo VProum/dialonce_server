98323251987938622225c65bdeae7718ba61e487
vincentpro <vincent_pro@hotmail.fr>
2021-08-13 09:49:17 +0200

    added route for read and delete and update react build

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,17 @@ router.get("/", function(req, res, next) {
     res.render("index", { title: "Express" });
 });
 
+router.get("/api/url", async(req, res, next) => {
+    try {
+        const dbResult = await Url.find();
+        console.log(dbResult);
+        res.status(200).json(dbResult);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
 router.post("/api/saveurl", async(req, res, next) => {
     try {
         const dbResult = await Url.create(req.body);
@@ -23,6 +34,17 @@ router.get("/:id", async(req, res, next) => {
         const dbResult = await Url.findById(req.params.id);
         console.log(dbResult);
         res.redirect(dbResult.longUrl);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+router.delete("/api/url/:id", async function(req, res, next) {
+    try {
+        await Url.findByIdAndDelete(req.params.id);
+        const dbResult = await Url.find();
+        res.status(200).json(dbResult);
     } catch (error) {
         console.log(error);
         next(error);
